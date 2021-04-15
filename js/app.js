278005436fa3dff86780fd377a39238b7afc1532
Oleg <oleg@localhost>
2021-04-15 18:45:25 +0400

    first commit

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
             const coordsEl = document.querySelector('#user-coords');
-            coordsEl.textContent = `${position.coords.latitude}, ${position.coords.longitude}`
+            coordsEl.textContent = `${position.coords.latitude},  ${position.coords.longitude}`
             coordsEl.parentElement.classList.remove('invisible');
         });
     }
